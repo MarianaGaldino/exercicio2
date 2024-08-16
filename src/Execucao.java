3be72143ad8277a326f521eb58476b69a84e004a
MarianaGaldino <marianagaldino1@gmail.com>
2024-08-15 21:54:13 -0300

    instanciado Fabrica de um novo jeito

--- src/Execucao.java
+++ src/Execucao.java
@@ -12,5 +12,22 @@ public class Execucao {
         System.out.println(fabrica1.getQuantidadeFiliais());
         System.out.println(fabrica1.getProprietário());
         System.out.println(fabrica1.getProduto());
+
+        Fabrica fabrica2 = new Fabrica();
+
+        fabrica2.setNome("Doce mel");
+        fabrica2.setCnpj("54321/0001");
+        fabrica2.setEndereco("Rua Popular");
+        fabrica2.setProduto("doce de leite");
+        fabrica2.setProprietário("Maria Joana");
+        fabrica2.setQuantidadeFiliais(0);
+        fabrica2.setQuantidadeFuncionarios(50);
+        fabrica2.setTelefone("45476890");
+        fabrica2.setSite("www.docemel.com");
+
+        System.out.println("Olá, eu trabalho na " + fabrica2.getNome() + " fica localizada na " + fabrica2.getEndereco()
+               + "\n a dona é a Sra " + fabrica2.getProprietário() + ". Temos um total de " + fabrica2.getQuantidadeFuncionarios() + " funcionarios " +
+               "\n voce pode entrar em contato pelo telefone " + fabrica2.getTelefone() + " ou no site " + fabrica2.getSite()
+                + "\n nós fabricamos " + fabrica2.getProduto() + " e temos um total de " + fabrica2.getQuantidadeFiliais() + " filiais");
     }
 }
